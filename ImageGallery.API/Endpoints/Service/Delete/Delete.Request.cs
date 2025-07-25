bd2314aee3c812b28e1b803501d96b90a0dc3d28
Main <sh.zamani2002@gmail.com>
2025-07-25 16:44:03 +0330

    feat: delete

---
+++ ImageGallery.API/Endpoints/Service/Delete/Delete.Request.cs
@@ -0,0 +1,7 @@
+namespace ImageGallery.API.Endpoints.Service.Delete;
+
+public class Request
+{
+    public const string Route = "Service";
+    public Guid Id { get; set; }    
+}
---
+++ ImageGallery.API/Endpoints/Service/Delete/Delete.cs
@@ -0,0 +1,26 @@
+using FastEndpoints;
+using FluentResults;
+using ImageGallery.Shared.Abstractions;
+
+namespace ImageGallery.API.Endpoints.Service.Delete;
+
+public class Delete : BaseEndpoint<Request, Result<bool>>
+{
+    public override void Configure()
+    {
+        Delete(Request.Route);
+        DontAutoTag();
+        Summary(s =>
+        {
+            s.Summary = "Delete Service By Id";
+            s.Description = "Delete Service By Id";
+        });
+    }
+
+    protected override async Task ExecuteAsync(Request request, CancellationToken ct)
+    {
+        var command = new Core.Services.Service.Delete(request.Id);
+        var result = await command.ExecuteAsync(ct);
+        await SendAsync(result);
+    }
+}
