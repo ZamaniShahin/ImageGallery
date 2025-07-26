523c18b4c81d7eecdf858cb3d9fdf626ddc0c6f3
Main <sh.zamani2002@gmail.com>
2025-07-26 12:37:00 +0330

    feat: add employee configurations, add preprocessor to DI

--- ImageGallery.Infrastructure/Persistence/Configs/EmployeeEntityConfigurations.cs
+++ ImageGallery.Infrastructure/Persistence/Configs/EmployeeEntityConfigurations.cs
@@ -4,9 +4,9 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace ImageGallery.Infrastructure.Persistence.Configs;
 
-public class ServiceConfigurations : IEntityTypeConfiguration<ServiceEntity>
+public class EmployeeEntityConfigurations : IEntityTypeConfiguration<EmployeeEntity>
 {
-    public void Configure(EntityTypeBuilder<ServiceEntity> builder)
+    public void Configure(EntityTypeBuilder<EmployeeEntity> builder)
     {
         builder.HasKey(e => e.Id);
 
@@ -18,9 +18,12 @@ public class ServiceConfigurations : IEntityTypeConfiguration<ServiceEntity>
             .HasMaxLength(LengthConstants.DescriptionMaxLength)
             .IsRequired();
         
-        builder
-            .Property(e => e.Price)
-            .HasColumnType("decimal(18,2)")
+        builder.Property(e => e.ProfilePhoto)
             .IsRequired();
+        
+        builder.HasOne(e => e.AboutUs)
+            .WithMany(e => e.Employees)
+            .HasForeignKey(e => e.AboutUsId)
+            .OnDelete(DeleteBehavior.NoAction);
     }
 }
